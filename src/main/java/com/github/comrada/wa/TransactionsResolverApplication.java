5ea2397653b9a720447e0288d1851307c0c3a768
Pavel Lobyrin <pavel@lobyrin.ru>
2021-12-30 22:51:55 +0100

    initial commit

---
+++ src/main/java/com/github/comrada/wa/TransactionsResolverApplication.java
@@ -0,0 +1,13 @@
+package com.github.comrada.wa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsResolverApplication.class, args);
+	}
+
+}
