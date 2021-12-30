3e527079e6bea26e270e5268d3638a40e4e83ba0
Pavel Lobyrin <pavel@lobyrin.ru>
2021-12-31 00:41:59 +0100

    added some JPA methods and tests for them

--- src/main/java/com/github/comrada/wa/repository/WhaleAlertRepository.java
+++ src/main/java/com/github/comrada/wa/repository/WhaleAlertRepository.java
@@ -1,10 +1,56 @@
 package com.github.comrada.wa.repository;
 
 import com.github.comrada.wa.entity.WhaleAlert;
+import com.github.comrada.wa.entity.WhaleAlert.ProcessingStatus;
+import java.time.Instant;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface WhaleAlertRepository extends JpaRepository<WhaleAlert, Long> {
 
+  @Query("select wa from WhaleAlert wa where wa.processStatus = :status order by wa.id")
+  List<WhaleAlert> findByStatus(ProcessingStatus status, Pageable pageable);
+
+  @Transactional
+  @Modifying(flushAutomatically = true)
+  @Query("""
+          update WhaleAlert wa set wa.processedAt = :processedAt,
+          wa.processAttempts = wa.processAttempts + 1 where wa.id = :id
+      """)
+  void incrementAttempt(long id, Instant processedAt);
+
+  @Transactional
+  @Modifying(flushAutomatically = true)
+  @Query("""
+          update WhaleAlert wa set wa.processedAt = :processedAt, wa.processStatus = :status,
+          wa.processAttempts = wa.processAttempts + 1 where wa.id = :id
+      """)
+  void setStatus(long id, ProcessingStatus status, Instant processedAt);
+
+  default List<WhaleAlert> findNewAlerts(int limit) {
+    return findByStatus(ProcessingStatus.NEW, PageRequest.of(0, limit));
+  }
+
+  default List<WhaleAlert> findFailedAlerts(int limit) {
+    return findByStatus(ProcessingStatus.FAILED, PageRequest.of(0, limit));
+  }
+
+  default List<WhaleAlert> findProcessedAlerts(int limit) {
+    return findByStatus(ProcessingStatus.DONE, PageRequest.of(0, limit));
+  }
+
+  default void done(long id, Instant processedAt) {
+    setStatus(id, ProcessingStatus.DONE, processedAt);
+  }
+
+  default void fail(long id, Instant processedAt) {
+    setStatus(id, ProcessingStatus.FAILED, processedAt);
+  }
 }
---
+++ src/test/resources/com/github/comrada/wa/repository/whale_alerts.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.whale_alerts (id, message, link, posted_at, asset, amount, process_status,
+                                 processed_at, process_attempts)
+-- new
+VALUES (1, '2,999 #PAXG (5,392,567 USD) transferred from #Binance to unknown wallet',
+        'https://whale-alert.io/transaction/ethereum/1dace3b2d84e6e0372f323afba5c414156d7c0c72509e3be16eae63985612db9/1',
+        '2021-12-20 10:04:01', 'PAXG', '2999', 'NEW', NULL, 0),
+       (10, '2,999 #PAXG (5,392,567 USD) transferred from #Binance to unknown wallet',
+        'https://whale-alert.io/transaction/ethereum/1dace3b2d84e6e0372f323afba5c414156d7c0c72509e3be16eae63985612db9/1',
+        '2021-12-20 10:04:02', 'PAXG', '2999', 'NEW', NULL, 0),
+-- failed
+       (2, '3,999 #PAXG (7,190,100 USD) transferred from #Binance to unknown wallet',
+        'https://whale-alert.io/transaction/ethereum/2206a09bdfbee5870005ccdfaafb70ba6b76127462689cad3e52e7c7a1f758a6/1',
+        '2021-12-20 10:04:00', 'PAXG', '3999', 'FAILED', '2021-12-21 00:00:00', 5),
+       (20, '3,999 #PAXG (7,190,100 USD) transferred from #Binance to unknown wallet',
+        'https://whale-alert.io/transaction/ethereum/2206a09bdfbee5870005ccdfaafb70ba6b76127462689cad3e52e7c7a1f758a6/1',
+        '2021-12-20 10:04:01', 'PAXG', '3999', 'FAILED', '2021-12-21 00:00:01', 5),
+-- done
+       (3, '60,000,000 #USDC (60,000,000 USD) transferred from unknown wallet to #CryptoCom',
+        'https://whale-alert.io/transaction/ethereum/ca08ac7675440bc7e016504d7fdc18b52c0c144a60a19f729d61c9ed405a7d68',
+        '2021-12-20 09:43:38', 'USDC', '60000000', 'DONE', '2021-12-21 00:00:10', 1),
+       (30, '60,000,000 #USDC (60,000,000 USD) transferred from unknown wallet to #CryptoCom',
+        'https://whale-alert.io/transaction/ethereum/ca08ac7675440bc7e016504d7fdc18b52c0c144a60a19f729d61c9ed405a7d68',
+        '2021-12-20 09:43:39', 'USDC', '60000000', 'DONE', '2021-12-21 00:00:11', 1);
